4e2feac12513fd3261706184e7c996d53a8d89cd
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2024-02-13 16:16:03 +0900

    fix: Remove canvaskit-wasm

--- src/client/components/product/ProductHeroImage/ProductHeroImage.tsx
+++ src/client/components/product/ProductHeroImage/ProductHeroImage.tsx
@@ -1,8 +1,4 @@
-import CanvasKitInit from 'canvaskit-wasm';
-import CanvasKitWasmUrl from 'canvaskit-wasm/bin/canvaskit.wasm?url';
 import classNames from 'classnames';
-import _ from 'lodash';
-import { memo, useEffect, useState } from 'react';
 import type { FC } from 'react';
 
 import type { ProductFragmentResponse } from '../../../graphql/fragments';
@@ -13,48 +9,13 @@ import { WidthRestriction } from '../../foundation/WidthRestriction';
 
 import * as styles from './ProductHeroImage.styles';
 
-async function loadImageAsDataURL(url: string): Promise<string> {
-  const CanvasKit = await CanvasKitInit({
-    // WASM ファイルの URL を渡す
-    locateFile: () => CanvasKitWasmUrl,
-  });
-
-  // 画像を読み込む
-  const data = await fetch(url).then((res) => res.arrayBuffer());
-  const image = CanvasKit.MakeImageFromEncoded(data);
-  if (image == null) {
-    // 読み込みに失敗したとき、透明な 1x1 GIF の Data URL を返却する
-    return 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
-  }
-
-  // 画像を Canvas に描画して Data URL を生成する
-  const canvas = CanvasKit.MakeCanvas(image.width(), image.height());
-  const ctx = canvas.getContext('2d');
-  // @ts-expect-error ...
-  ctx?.drawImage(image, 0, 0);
-  return canvas.toDataURL();
-}
-
 type Props = {
   product: ProductFragmentResponse;
   title: string;
 };
 
-export const ProductHeroImage: FC<Props> = memo(({ product, title }) => {
+export const ProductHeroImage: FC<Props> = ({ product, title }) => {
   const thumbnailFile = product.media.find((productMedia) => productMedia.isThumbnail)?.file;
-
-  const [imageDataUrl, setImageDataUrl] = useState<string>();
-
-  useEffect(() => {
-    if (thumbnailFile == null) {
-      return;
-    }
-    loadImageAsDataURL(thumbnailFile.filename).then((dataUrl) => setImageDataUrl(dataUrl));
-  }, [thumbnailFile]);
-
-  if (imageDataUrl === undefined) {
-    return null;
-  }
 
   return (
     <GetDeviceType>
@@ -64,7 +25,7 @@ export const ProductHeroImage: FC<Props> = memo(({ product, title }) => {
             <Anchor href={`/product/${product.id}`}>
               <div className={styles.container()}>
                 <AspectRatio ratioHeight={9} ratioWidth={16}>
-                  <img className={styles.image()} src={imageDataUrl} />
+                  <img className={styles.image()} src={thumbnailFile?.filename} />
                 </AspectRatio>
 
                 <div className={styles.overlay()}>
@@ -92,6 +53,4 @@ export const ProductHeroImage: FC<Props> = memo(({ product, title }) => {
       }}
     </GetDeviceType>
   );
-}, _.isEqual);
-
-ProductHeroImage.displayName = 'ProductHeroImage';
+};
